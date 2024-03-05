31a35bff0dc3f2aeeceb20e5480da28111fd7298
Bilguun04 <bilguunbek0607@gmail.com>
2024-03-05 17:36:28 -0500

    Update legalmove.py

--- legalmove.py
+++ legalmove.py
@@ -5,7 +5,8 @@ class LegalMove:
     def __init__(self, board):
         self.board = board
 
-    def isLegal(self, piece, initial, final):
+    def isLegal(self, initial, final):
+        piece = self.board.board[initial[0]][initial[1]].piece
         in_row = initial[0]
         in_col = initial[1]
         fin_row = final[0]
@@ -27,10 +28,23 @@ class LegalMove:
                 elif abs(in_col - fin_col) == 1 and fin_row - in_row == 1 and not self.board.board[fin_row][fin_col].isEmpty() and self.board.board[fin_row][fin_col].piece.iswhite():
                     return True
 
+        elif type(piece) == Knight:
+            if abs(in_row - fin_row) == 2 and abs(in_col - fin_col) == 1 and (self.board.board[fin_row][fin_col].isEmpty() or self.board.board[fin_row][fin_col].piece.white != self.board.board[in_row][in_col].piece.white):
+                return True
+            elif abs(in_row - fin_row) == 1 and abs(in_col - fin_col) == 2 and (self.board.board[fin_row][fin_col].isEmpty() or self.board.board[fin_row][fin_col].piece.white != self.board.board[in_row][in_col].piece.white):
+                return True
 
-
-
-
+        elif type(piece) == Bishop:
+            directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+            for dr, dc in directions:
+                for distance in range (8):
+                    row, col = in_row + dr*distance, in_col + dc*distance
+                    if not (0 <= row < 8 and 0 <= col < 8):
+                        break
+                    if self.board.board[row][col].isEmpty():
+                        if row == fin_row and col == fin_col:
+                            return True
+                
         elif type(piece) == Rook:
             if in_row == fin_row and in_col != fin_col:
                 if in_col < fin_col:
@@ -55,96 +69,30 @@ class LegalMove:
                             return False
                     return True
             return False
-                
 
-
-
-
-
-
-
-
-        elif type(piece) == Knight:
-            if abs(in_row - fin_row) == 2 and abs(in_col - fin_col) == 1:
-                return True
-            elif abs(in_row - fin_row) == 1 and abs(in_col - fin_col) == 2:
-                return True
-        elif type(piece) == Bishop:
-            if abs(in_row - fin_row) == abs(in_col - fin_col):
-                if in_row < fin_row and in_col < fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row + i][in_col + i].isEmpty():
-                            return False
-                    return True
-                elif in_row < fin_row and in_col > fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row + i][in_col - i].isEmpty():
-                            return False
-                    return True
-                elif in_row > fin_row and in_col < fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row - i][in_col + i].isEmpty():
-                            return False
-                    return True
-                elif in_row > fin_row and in_col > fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row - i][in_col - i].isEmpty():
-                            return False
-                    return True
         elif type(piece) == Queen:
             if in_row == fin_row and in_col != fin_col:
                 if in_col < fin_col:
-                    for i in range(in_col + 1, fin_col):
-                        if not self.board.board[in_row][i].isEmpty():
+                    for col in range (in_col, fin_col+1):
+                        if not self.board.board[in_row][col].isEmpty():
                             return False
-                    return True
                 else:
-                    for i in range(fin_col + 1, in_col):
-                        if not self.board.board[in_row][i].isEmpty():
+                    for col in range (in_col, fin_col-1, -1):
+                        if not self.board.board[in_row][col].isEmpty():
                             return False
-                    return True
-            elif in_col == fin_col and in_row != fin_row:
-                if in_row < fin_row:
-                    for i in range(in_row + 1, fin_row):
-                        if not self.board.board[i][in_col].isEmpty():
-                            return False
-                    return True
-                else:
-                    for i in range(fin_row + 1, in_row):
-                        if not self.board.board[i][in_col].isEmpty():
-                            return False
-                    return True
-            elif abs(in_row - fin_row) == abs(in_col - fin_col):
-                if in_row < fin_row and in_col < fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row + i][in_col + i].isEmpty():
-                            return False
-                    return True
-                elif in_row < fin_row and in_col > fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row + i][in_col - i].isEmpty():
-                            return False
-                    return True
-                elif in_row > fin_row and in_col < fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row - i][in_col + i].isEmpty():
-                            return False
-                    return True
-                elif in_row > fin_row and in_col > fin_col:
-                    for i in range(1, abs(in_row - fin_row)):
-                        if not self.board.board[in_row - i][in_col - i].isEmpty():
-                            return False
-                    return True
+                return True
+
         elif type(piece) == King:
             if abs(in_row - fin_row) <= 1 and abs(in_col - fin_col) <= 1:
                 return True
     
-    def generate_legal_moves_piece(self, piece, initial):
+    def generate_legal_moves_piece(self, initial):
         legal_moves = []
         for i in range(8):
             for j in range(8):
-                if self.isLegal(piece, initial, [i, j]) and (self.board.board[i][j].isEmpty() or self.board.board[i][j].piece.iswhite() != piece.iswhite()):
+                if self.isLegal(initial, [i, j]):
                     legal_moves.append([i, j])
+        legal_moves = sorted(legal_moves)
         return legal_moves
 
     def generate_legal_move_board(self):
--- test/test_legal_move.py
+++ test/test_legal_move.py
@@ -3,55 +3,56 @@ from legalmove import *
 from board import *
 
 class TestLegalMove(unittest.TestCase):
-    def test_legal_move_initial(self):
-        board = Board()
+    def setUp(self):
+        self.board = Board()
+        self.legalmove = LegalMove(self.board)
 
-        r = LegalMove(board)
-        legalmove = r.generate_legal_moves(board.board[0][0].piece, [0, 0])
-        self.assertEqual(legalmove, [])
+    def test_pawn_legal_move(self):
+        self.assertEqual(self.legalmove.generate_legal_moves_piece([1, 0]), [[2, 0], [3, 0]])
+        self.assertEqual(self.legalmove.generate_legal_moves_piece([6, 0]), [[4, 0], [5, 0]])
 
-        n = LegalMove(board)
-        legalmove = n.generate_legal_moves(board.board[0][1].piece, [0, 1])
-        self.assertEqual(legalmove, [[2, 0], [2, 2]])
-
-        b = LegalMove(board)
-        legalmove = b.generate_legal_moves(board.board[0][2].piece, [0, 2])
-        self.assertEqual(legalmove, [])
-
-        q = LegalMove(board)
-        legalmove = q.generate_legal_moves(board.board[0][3].piece, [0, 3])
-        self.assertEqual(legalmove, [])
-
-        k = LegalMove(board)
-        legalmove = k.generate_legal_moves(board.board[0][4].piece, [0, 4])
-        self.assertEqual(legalmove, [])
-
-        b = LegalMove(board)
-        legalmove = b.generate_legal_moves(board.board[0][5].piece, [0, 5])
-        self.assertEqual(legalmove, [])
-
-        n = LegalMove(board)
-        legalmove = n.generate_legal_moves(board.board[0][6].piece, [0, 6])
-        self.assertEqual(legalmove, [[2, 5], [2, 7]])
-
-        r = LegalMove(board)
-        legalmove = r.generate_legal_moves(board.board[0][7].piece, [0, 7])
-        self.assertEqual(legalmove, [])
-
-        for col in range (8):
-            p = LegalMove(board)
-            legalmove = p.generate_legal_moves(board.board[1][col].piece, [1, col])
-            self.assertEqual(legalmove, [[2, col], [3, col]])
+        self.board.board[3][0].piece = Pawn(False)
+        self.assertEqual(self.legalmove.generate_legal_moves_piece([1, 0]), [[2, 0]])
     
-    def test_rook_legal_moves(self):
-        board = Board()
-        legalmove = LegalMove(board)
+    def test_knight_legal_move(self):
+        self.board.empty()
+        self.board.board[3][3].piece = Knight(False)
+        self.assertEqual(self.legalmove.generate_legal_moves_piece([3, 3]), [[1, 2], [1, 4], [2, 1], [2, 5], [4, 1], [4, 5], [5, 2], [5, 4]])
+        
+        self.board.board[2][1].piece = Pawn(False)
+        self.assertEqual(self.legalmove.generate_legal_moves_piece([3, 3]), [[1, 2], [1, 4], [2, 5], [4, 1], [4, 5], [5, 2], [5, 4]])
+    
+    def test_bishop_legal_move(self):
+        self.board.empty()
+        self.board.board[3][3].piece = Bishop(False)
+        self.assertEqual(self.legalmove.generate_legal_moves_piece([3, 3]), sorted([[0, 0], [1, 1], [2, 2], [4, 4], [5, 5], [6, 6], [7, 7],
+                                                                           [0, 6], [1, 5], [2, 4], [4, 2], [5, 1], [6, 0]]))
+        
+        self.board.board[2][2].piece = Pawn(False)
+        #self.assertEqual(self.legalmove.generate_legal_moves_piece([3, 3]), sorted([[4, 4], [5, 5], [6, 6], [7, 7], [0, 6], [1, 5], [2, 4], [4, 2], [5, 1], [6, 0]]))
+        #self.assertEqual(len(self.legalmove.generate_legal_moves_piece([3, 3])), 10)
+        print(sorted(self.legalmove.generate_legal_moves_piece([3, 3])))
+        self.board.display()
 
-        board.empty()
-        board.board[0][0] = Rook(True)
-        self.assertTrue(legalmove.isLegal(board.board[0][0], [0, 0], [0, 7]))
+    def test_king_legal_move(self):
+        self.board.empty()
+        self.board.board[3][3].piece = King(False)
+        self.assertEqual(self.legalmove.generate_legal_moves_piece([3, 3]), [[2, 2], [2, 3], [2, 4], [3, 2], [3, 4], [4, 2], [4, 3], [4, 4]])
 
-        board.board[0][4] = Rook(True)
-        board.display()
+        self.board.board[2][2].piece = Pawn(False)
+        #self.assertEqual(self.legalmove.generate_legal_moves_piece([3, 3]), [[2, 3], [2, 4], [3, 2], [3, 4], [4, 2], [4, 3], [4, 4]])
+    
+    def test_queen_legal_move(self):
+        self.board.empty()
+        self.board.board[3][3].piece = Queen(False)
+        #self.assertEqual(self.legalmove.generate_legal_moves_piece(self.board.board[3][3].piece, [3, 3]), [[0, 3], [1, 3], [2, 3], [4, 3], [5, 3], [6, 3], [7, 3],
+        #                                                                                                   [0, 0], [1, 1], [2, 2], [4, 4], [5, 5], [6, 6], [7, 7],
+        #                                                                                                   [0, 6], [1, 5], [2, 4], [4, 2], [5, 1], [6, 0],
+        #                                                                                                   [3, 0], [3, 1], [3, 2], [3, 4], [3, 5], [3, 6], [3, 7]])
+        #self.assertEqual(len(self.legalmove.generate_legal_moves_piece(self.board.board[3][3].piece, [3, 3])), 23)
 
-        self.assertFalse(legalmove.isLegal(board.board[0][0], [0, 0], [0, 7]))
+        #self.board.board[3][4].piece = Pawn(False)
+        print(len(self.legalmove.generate_legal_moves_piece([3, 3])))
+        for item in self.legalmove.generate_legal_moves_piece([3, 3]):
+            print(item)
+        self.board.display()
